d18341465dbd7fac21dbe397617faff2039e2b45
Matheus Cerqueira <matheus.cerqueira1208@.com>
2021-06-25 10:05:38 -0300

    Authtication created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContextProvider } from './contexts/AuthContext';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
-    <h1> Hello World</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+      <Route path="/rooms/:is" component={Room}/>
+
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
